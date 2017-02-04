1d4715dd1e7286174220c9dba476925c5487b65d
Evan Morrison <evanrmorrison@gmail.com>
2017-02-03 20:26:45 -0700

    add serverjs

---
+++ server.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, '/dist')));
+
+app.listen(port, function() {
+  console.log('Server is listening on port ', port);
+});
