1b5f309845d6878f480de1bd9d04eeb59838fea2
Evan Morrison <evanrmorrison@gmail.com>
2017-02-08 14:08:28 -0700

    update webpack build config

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -21,6 +21,15 @@ module.exports = {
             {test:/\.css$/, loader:"style-loader!css-loader"}
         ]
     },
-    plugins: [new webpack.HotModuleReplacementPlugin(), HTMLWebpackPluginConfig]
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
     
 }
