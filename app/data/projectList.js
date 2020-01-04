ebe628f0719148d3b3a789d657e521c9fd5bfe75
Evan Morrison <evanrmorrison@gmail.com>
2020-01-03 23:39:12 -0700

    remove old projects from portfolio

--- app/data/projectList.js
+++ app/data/projectList.js
@@ -50,61 +50,44 @@ const projectList = [
         imgUrl: projectUrls['Real-T'],
         gitUrl: "https://github.com/EvanMorrison/real-t",
         frameworks: ['AngularJS', 'AngularMaterial', 'UIRouter', 'Webpack', 'Sass', 'Nodejs', 'MongoDB']
-      },{
-        name: "Book Reviews",
-        siteUrl: "http://bookreviewspa.evmorrison.com",
-        imgUrl: projectUrls['Book Lovers'],
-        gitUrl: "https://github.com/EvanMorrison/BookReviewSite",
-        frameworks: ['AngularJS', 'Bootstrap', 'Nodejs', 'MongoDB']
-      },{
-        name: "Mad-Lib",
-        siteUrl: "http://evmorrison.com/sampleprojects/angularMadlib",
-        imgUrl: projectUrls['Mad-Lib'],
-        gitUrl: "https://github.com/EvanMorrison/angularjs-madLib",
-        frameworks: ['AngularJS', 'Bootstrap']
-      },{
-        name: "Mario's Earnings (CSS Animation)",
-        siteUrl: "http://evmorrison.com/sampleprojects/mario",
-        imgUrl: projectUrls['Mario Pest Control'],
-        gitUrl: "https://github.com/EvanMorrison/Mario-Pest-Control",
-        frameworks: ['HTML5', 'CSS3', 'jQuery']
-      },{
-        name: "Shopping List",
-        siteUrl: "http://evmorrison.com/sampleprojects/shoppinglist",
-        imgUrl: projectUrls['Shopping List'],
-        gitUrl: "https://github.com/EvanMorrison/Mothers-Shopping-List",
-        frameworks: ['jQuery', 'Bootstrap']
-      },{
-        name: "Pokemon Profiles (type ahead, with sprites)",
-        siteUrl: "http://evmorrison.com/sampleprojects/pokeprofiles",
-        imgUrl: projectUrls['Pokemon Profiles'],
-        gitUrl: "https://github.com/EvanMorrison/pokeProfile",
-        frameworks: ['AngularJS', 'AngularUI', 'Bootstrap']
-      },{
-        name: "Pong JS (HTML5 canvas)",
-        siteUrl: "http://evmorrison.com/sampleprojects/pongjs",
-        imgUrl: projectUrls['PongJS'],
-        gitUrl: "#",
-        frameworks: ['HTML5', 'CSS3', 'JavaScript']
-      },{
-        name: "CarView Project",
-        siteUrl: "#",
-        imgUrl: projectUrls['CarView Project'],
-        gitUrl: "https://github.com/EvanMorrison/carShowDB",
-        frameworks: ['AngularJS', 'Angular UI', 'Bootstrap', 'express', 'Nodejs']
-      },{
-        name: "Everything is Dragable",
-        siteUrl: "#",
-        imgUrl: projectUrls['Everything is Dragable'],
-        gitUrl: "https://github.com/EvanMorrison/everything-is-dragable",
-        frameworks: ['AngularJS']
-      },{
-        name: "ToDo in Angular",
-        siteUrl: "#",
-        imgUrl: projectUrls['ToDo in Angular'],
-        gitUrl: "https://github.com/EvanMorrison/AngularJS-toDoWebsite",
-        frameworks: ['AngularJS']
-      }
+      },
+      // {
+      //   name: "Book Reviews",
+      //   siteUrl: "http://bookreviewspa.evmorrison.com",
+      //   imgUrl: projectUrls['Book Lovers'],
+      //   gitUrl: "https://github.com/EvanMorrison/BookReviewSite",
+      //   frameworks: ['AngularJS', 'Bootstrap', 'Nodejs', 'MongoDB']
+      // },{
+      //   name: "Mad-Lib",
+      //   siteUrl: "http://evmorrison.com/sampleprojects/angularMadlib",
+      //   imgUrl: projectUrls['Mad-Lib'],
+      //   gitUrl: "https://github.com/EvanMorrison/angularjs-madLib",
+      //   frameworks: ['AngularJS', 'Bootstrap']
+      // },{
+      //   name: "Mario's Earnings (CSS Animation)",
+      //   siteUrl: "http://evmorrison.com/sampleprojects/mario",
+      //   imgUrl: projectUrls['Mario Pest Control'],
+      //   gitUrl: "https://github.com/EvanMorrison/Mario-Pest-Control",
+      //   frameworks: ['HTML5', 'CSS3', 'jQuery']
+      // },{
+      //   name: "Shopping List",
+      //   siteUrl: "http://evmorrison.com/sampleprojects/shoppinglist",
+      //   imgUrl: projectUrls['Shopping List'],
+      //   gitUrl: "https://github.com/EvanMorrison/Mothers-Shopping-List",
+      //   frameworks: ['jQuery', 'Bootstrap']
+      // },{
+      //   name: "Pokemon Profiles (type ahead, with sprites)",
+      //   siteUrl: "http://evmorrison.com/sampleprojects/pokeprofiles",
+      //   imgUrl: projectUrls['Pokemon Profiles'],
+      //   gitUrl: "https://github.com/EvanMorrison/pokeProfile",
+      //   frameworks: ['AngularJS', 'AngularUI', 'Bootstrap']
+      // },{
+      //   name: "Pong JS (HTML5 canvas)",
+      //   siteUrl: "http://evmorrison.com/sampleprojects/pongjs",
+      //   imgUrl: projectUrls['PongJS'],
+      //   gitUrl: "#",
+      //   frameworks: ['HTML5', 'CSS3', 'JavaScript']
+      // }
       ]
 
 
